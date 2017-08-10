75f38dcc69a23d0e1ca67456d0353a3891424ec1
Avindu Hewa <avinduhewa@gmail.com>
2017-08-11 01:29:36 +0530

    React project setup

---
+++ api/Dashboards/handlers/getAllDashboards/getAllDashboards.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const database = require('../../lib/database');
+const returnData = require('../../lib/return').returnData;
+
+module.exports.hello = (event, context, callback) => {
+  const data = JSON.parse(event.body);
+  database.initDB(db => {
+
+    const USERS = db.collection('Users');
+    const DASHBOARDS = db.collection('Dashboards');
+
+    DASHBOARDS.find({ _id: { $in: data.dashboards } })
+      .then(result => {
+        return returnData({ status: 200, data: result });
+      })
+      .catch(err => returnData({ status: 400, data: err }, context))
+  })
+};
