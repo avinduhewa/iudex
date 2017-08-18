0bb6a5792575316a8a9b47b36576bb7b601b6f2a
Avindu Hewa <avinduhewa@gmail.com>
2017-08-18 23:06:15 +0530

    added getCommittee api endpoint

---
+++ api/Dashboards/handlers/getCommittee/getCommittee.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const returnData = require('../../lib/return').returnData;
+const helper = require('./helper');
+const db = require('../../lib/database');
+
+const initDB = db.initDB;
+const ObjectId = db.objectID;
+
+module.exports.getCommittee = (event, context, callback) => {
+  const data = event.queryStringParameters;
+
+  initDB(db => {
+    const com = db.collection('Committees');
+
+    com.findOne({ _id: ObjectId(data.committee) })
+      .then(comData => {
+        const normalizedCommittee = this.normalizeCommittee(comData);
+        return returnData({ status: 200, data: comData }, context);
+      })
+      .catch(err => returnData({ status: 402, data: err }, context));
+  })
+};
+
+module.exports.normalizeCommittee = (comData) => {
+  for (let x in comData.countries){
+    delete comData.countries[x].points;
+  }
+  return comData;
+}
