359791e6ec7e646960ec66f20ccd4bb2b72b23c1
Peshala <peshaladayanatha@gmail.com>
2017-08-19 08:56:36 +0530

    begining of the admin components

--- app/src/components/DashboardAdminView/DashboardAdminComponents/DashboardAdminSelect.js
+++ app/src/components/DashboardAdminView/DashboardAdminComponents/DashboardAdminSelect.js
@@ -1,28 +1,118 @@
 import React, { Component } from 'react';
+import './accordion.css';
+
+
 
 
 
 class DashboardAdminSelect extends Component {
+
+  handleClick() {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    debugger;
+    console.log(acc)
+    for (i = 0; i < acc.length; i++) {
+      debugger;
+      acc[i].onclick = function () {
+        debugger;
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      }
+    }
+  }
+
+
   render() {
     return (
-      <div className="DashboardAdminSelect bodyCards">
-        
-       
-        <div className="ui card">
-        <div className="content">
-          <div className="header">Project Timeline</div>
-        </div>
-        <div className="content">
-          fhgfhghgfhg
-        </div>
-        <div className="extra content">
-          <button className="ui button">Join Project</button>
-        </div>
-      </div>
-     
+      <div className="DashboardAdminSelect ">
 
-    </div>
-    
+        <div className="ui card " id="activityCard">
+          <div className="content">
+            <div className="header">Deligates and Points</div>
+          </div>
+          <div className="content" onClick={this.handleClick}>
+            <button className="accordion" >Avindu</button>
+            <div className="panel">
+              <div className="ui relaxed divided list">           
+                <div className="item">
+                <i className="large user circle middle aligned icon"></i>
+                  <div className="content">
+                    <div className="ui list">
+                      <div className="item">
+                        <div className="content">
+                          <div className="header">Avindu</div>
+                          <div className="description">Avindu@gmail.com</div>
+                          <div className="list">
+                            <div className="ui list">
+                              <div className="item">
+                                <div class="ui divider"></div>
+                                <div className="content">
+                                  <div className="header">Debating - {50}</div>
+                                  <div className="header">Lobbying - {20}</div>
+                                  <div className="header">Knowledge of protocol - {40} </div>
+                                  <div className="header">Foreign policy statement - {60}</div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>     
+              </div>
+            </div>
+{/* hardcoded just to show */}
+            <button className="accordion" >Peshala</button>
+            <div className="panel">
+              <div className="ui relaxed divided list">           
+                <div className="item">
+                <i className="large user circle middle aligned icon"></i>
+                  <div className="content">
+                    <div className="ui list">
+                      <div className="item">
+                        <div className="content">
+                          <div className="header">Peshala Rasanga</div>
+                          <div className="description">Peshalarasanga@gmail.com</div>
+                          <div className="list">
+                            <div className="ui list">
+                              <div className="item">
+                                <div class="ui divider"></div>
+                                <div className="content">
+                                  <div className="header">Debating - {20}</div>
+                                  <div className="header">Lobbying - {20}</div>
+                                  <div className="header">Knowledge of protocol - {20} </div>
+                                  <div className="header">Foreign policy statement - {20}</div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>     
+              </div>
+            </div>
+          </div>
+          <div className="extra content">
+            {/* <button className="ui button">Join Project</button> */}
+          </div>
+        </div>
+
+
+      </div>
+
     );
   }
 }
--- app/src/components/DashboardAdminView/DashboardAdminComponents/DashboardAdminView.js
+++ app/src/components/DashboardAdminView/DashboardAdminComponents/DashboardAdminView.js
@@ -8,60 +8,18 @@ class DashboardAdminView extends Component {
   render() {
     return (
       <div className="DashboardAdminView ">
-          <table className="ui celled structured table">
-            <thead>
-                <tr>
-                <th rowSpan="2">Name</th>
-                <th rowSpan="2">Type</th>
-                <th rowSpan="2">Files</th>
-                <th colSpan="3">Languages</th>
-                </tr>
-                <tr>
-                <th>Ruby</th>
-                <th>JavaScript</th>
-                <th>Python</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <td>Alpha Team</td>
-                <td>Project 1</td>
-                <td className="right aligned">2</td>
-                <td className="center aligned">
-                    <i className="large green checkmark icon"></i>
-                </td>
-                
-                </tr>
-                <tr>
-                <td rowspan="3">Beta Team</td>
-                <td>Project 1</td>
-                <td className="right aligned">52</td>
-                <td className="center aligned">
-                    <i className="large green checkmark icon"></i>
-                </td>
-               
-                </tr>
-                <tr>
-                <td>Project 2</td>
-                <td className="right aligned">12</td>
-                <td></td>
-                <td className="center aligned">
-                    <i className="large green checkmark icon"></i>
-                </td>
-               
-               
-                </tr>
-                <tr>
-                <td>Project 3</td>
-                <td className="right aligned">21</td>
-                <td className="center aligned">
-                    <i className="large green checkmark icon"></i>
-                </td>
-                <td></td>
-               
-                </tr>
-            </tbody>
-            </table>
+
+          <div className="ui card " id="activityCard">
+            <div className="content">
+              <div className="header">Activity</div>
+            </div>
+            <div className="content">
+         
+            </div>
+            <div className="extra content">
+              {/* <button className="ui button">Join Project</button> */}
+            </div>
+          </div>
    
       </div>
     );
