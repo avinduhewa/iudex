a4cd81acf9d1722b16bcbc11a7100d46eba316cf
Peshala <peshaladayanatha@gmail.com>
2017-08-20 13:04:08 +0530

    major changes

--- adminapp/src/App.js
+++ adminapp/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import SideNavBar from './components/SideNavBar';
+import DashboardAdmin from './components/DashboardAdminView/dashboarMainView';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SideNavBar/>
+        <DashboardAdmin/>
+
+
       </div>
     );
   }
---
+++ deligateapp/src/components/DashboardViewComponent/DashboardViewGrid.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+
+
+
+
+
+class DashboardViewGrid extends Component {
+
+
+
+  render() {
+    return (
+      <div className ="DashboardViewGrid bodyCards">
+
+      <div className ="ui container">
+asdads
+        </div>
+
+
+
+
+   
+      </div>
+    );
+  }
+}
+
+export default DashboardViewGrid;
