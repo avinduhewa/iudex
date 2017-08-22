bf970d92716784fde703a8648d41d58893ccbe14
Peshala <peshaladayanatha@gmail.com>
2017-08-23 05:13:59 +0530

    deligate app

--- deligateapp/src/components/DashboardViewComponent/DashboardViewGrid.js
+++ deligateapp/src/components/DashboardViewComponent/DashboardViewGrid.js
@@ -7,7 +7,30 @@ import React, { Component } from 'react';
 
 class DashboardViewGrid extends Component {
 
+  constructor(props) {
+    super(props);
 
+    this.state = {
+
+    }
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+   
+    
+
+}
+onChange() {
+ 
+  console.log("changigns")
+}
+
+onSubmit(e) {
+  e.preventDefault();
+  console.log(this.refs.Committee.value);
+  
+ 
+}
 
   render() {
     return (
@@ -21,23 +44,23 @@ class DashboardViewGrid extends Component {
     </div>
   </h2>
   
-  <form  className="ui  form">
+  <form  className="ui  form" onSubmit={this.onSubmit}>
     <div className="ui stacked secondary  segment">
       <div className="field">
         <div className="ui left icon input">       
-          <input type="text" placeholder=" Committee " name="committee"/>
+          <input type="text" placeholder=" Committee" ref="Committee"  name="committee" onChange = {this.onChange}/>
+        </div>
+      </div>
+      {/* <div className="field">
+        <div className="ui left icon input">  
+          <input type="text" name="country" ref="country" value={this.state.value} placeholder="Country" onChange = {this.onChange}/>
         </div>
       </div>
       <div className="field">
         <div className="ui left icon input">  
-          <input type="text" name="country" placeholder="Country"/>
+          <input type="text" name="country" ref="Committee" placeholder="Country"/>
         </div>
-      </div>
-      <div className="field">
-        <div className="ui left icon input">  
-          <input type="text" name="country" placeholder="Country"/>
-        </div>
-      </div>
+      </div> */}
       <button className="ui fluid   submit button">Submit</button>
     </div>
 
