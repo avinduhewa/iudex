94c1f87ccb1ce10504719ad7f22e6cf02a85cb5a
avinduhewa <avinduhewa@gmail.com>
2018-08-25 03:22:04 +0530

    qweq

--- iudex v2/src/app/secure/dashboard/core-comp/core-comp.component.ts
+++ iudex v2/src/app/secure/dashboard/core-comp/core-comp.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { CountdownTimerModule } from 'ngx-countdown-timer';
 
 @Component({
   selector: 'app-core-comp',
@@ -8,7 +11,18 @@ import { Component, OnInit } from '@angular/core';
 export class CoreCompComponent implements OnInit {
   selectedImagesArray = [];
   image = [];
-  public activeStatus =  true;
+  public activeStatus = true;
+  form: FormGroup;
+  points: any = [];
+  criteria: any = [];
+  county: any = [
+    {
+      '1': 'tesitnf'
+    },
+    {
+      '2': 'adsd'
+    }
+  ];
   collection = [
     {
       "imageName": "Screenshot 2018-07-03 13.28.32.png",
@@ -16,57 +30,77 @@ export class CoreCompComponent implements OnInit {
       "context": "lit vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje it vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje it vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje?",
       "test1": "testing",
       "test2": "testing",
-    }, 
+    },
     {
       "imageName": "Screenshot 2018-07-03 13.28.32.png",
       "ModifiedDate": "2",
       "context": "lit vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje it vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje it vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje?",
       "test1": "testing",
       "test2": "testing",
-    }, 
+    },
     {
       "imageName": "Screenshot 2018-07-03 13.28.32.png",
       "ModifiedDate": "2",
       "context": "lit vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje it vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje it vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje?",
       "test1": "testing",
       "test2": "testing",
-    }, 
+    },
     {
       "imageName": "Screenshot 2018-07-03 13.28.32.png",
       "ModifiedDate": "2",
       "context": "lit vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje it vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje it vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje?",
       "test1": "testing",
       "test2": "testing",
-    }, 
+    },
     {
       "imageName": "Screenshot 2018-07-03 13.28.32.png",
       "ModifiedDate": "2",
       "context": "lit vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje it vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje it vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje?",
       "test1": "testing",
       "test2": "testing",
-    }, 
+    },
     {
       "imageName": "Screenshot 2018-07-03 13.28.32.png",
       "ModifiedDate": "2",
       "context": "lit vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje it vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje it vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje?",
       "test1": "testing",
       "test2": "testing",
-    }, 
+    },
     {
       "imageName": "Screenshot 2018-07-03 13.28.32.png",
       "ModifiedDate": "2",
       "context": "lit vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje it vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje it vero! Excepturi, explicabo, laborvmnvmvmnbbnbnmbhhjvjvjhvhjvjjhvjvjjjhvhjvjje?",
       "test1": "testing",
       "test2": "testing",
-    },   
+    },
   ]
-  constructor() { }
+
+
+  constructor(
+
+  ) {
+
+  }
 
   selectRow() {
     this.activeStatus = !this.activeStatus;
   }
 
+  submitPoints(form) {
+    console.log('user is ', form);
+    // this.pollingService.addUser(user).subscribe(res => {
+    //   console.log('done');
+    // });
+  }
+
   ngOnInit() {
+
+    this.form = new FormGroup({
+      country: new FormControl(''),
+      criteria: new FormControl(''),
+      points: new FormControl('')
+    });
+
     console.log('core cmp works!');
   }
 
--- iudex v2/src/app/secure/dashboard/dashboard.module.ts
+++ iudex v2/src/app/secure/dashboard/dashboard.module.ts
@@ -5,13 +5,17 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { CoreCompComponent } from './core-comp/core-comp.component';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { TruncateModule } from 'ng2-truncate';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { CountdownTimerModule } from 'ngx-countdown-timer';
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardRoutingModule,
     NgxPaginationModule,
-    TruncateModule
+    TruncateModule,
+    NgSelectModule,
+    CountdownTimerModule.forRoot()
   ],
   declarations: [
     CoreCompComponent,
